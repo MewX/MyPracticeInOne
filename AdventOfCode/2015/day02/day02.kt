37dee86e5807c788133f248f3a378e8b5a027957
MewX <imewx@qq.com>
2016-11-17 18:34:22 +1030

    Added advent of code 2015 day 02 part 2

--- AdventOfCode/2015/day02/day02.kt
+++ AdventOfCode/2015/day02/day02.kt
@@ -1,12 +1,16 @@
 fun main(args: Array<String>) {
-    var sum = 0;
+    var sum = 0
+    var sum2 = 0
     for (case in input) {
         val nums: List<String> = case.split("x")
-        val a = nums[0].toInt()
-        val b = nums[1].toInt()
-        val c = nums[2].toInt()
-        val result: List<Int> = listOf(a * b, a * c, b * c)
-        sum += result.sum().shl(1) + result.min()!!
+        val a: Int = nums[0].toInt()
+        val b: Int = nums[1].toInt()
+        val c: Int = nums[2].toInt()
+        val result: List<Int> = listOf(a * b, a * c, b * c).sorted()
+        val result2: List<Int> = listOf(a, b, c).sorted()
+        sum += result.sum().shl(1) + result[0]
+        sum2 += (result2[0] + result2[1]).shl(1) + a * b * c
     }
-    println(sum)
+    println("part1: $sum")
+    println("part2: $sum2")
 }
