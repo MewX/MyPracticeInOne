bbfa4afeb86eee265ff358ce931ce0aee5633763
MewX <imewx@qq.com>
2016-11-27 21:32:56 +1030

    Added day05 part2, AOC

--- AdventOfCode/2015/day05/Day05.kt
+++ AdventOfCode/2015/day05/Day05.kt
@@ -5,4 +5,24 @@ fun main(args: Array<String>) {
         && it.fold(Pair<Boolean, Char>(false, ' ')) { p, ch -> Pair<Boolean, Char>(p.first || p.second == ch, ch) }.first
     }
     println("part1: $num1")
+
+    val num2 = input.count {
+        it.let {
+            var result1 = false
+            for (i in 0..it.length - 2)
+                if (it.lastIndexOf(it.substring(i, i + 2)) - i >= 2)
+                    result1 = true
+
+            if (result1) {
+                var result2 = false
+                "abcdefghijklmnopqrstuvwxyz".toList().forEach {
+                    c ->
+                    if (it.contains(Regex("$c.$c")))
+                        result2 = true
+                }
+                result2
+            } else false
+        }
+    }
+    println("part2: $num2")
 }
