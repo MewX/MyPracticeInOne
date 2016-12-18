e3fef4a8082288dc947485c931d30c99f6408088
MewX <imewx@qq.com>
2016-12-18 18:41:57 +1030

    Added an exception for Hamming

--- exercism.io/javascript/hamming/hamming.js
+++ exercism.io/javascript/hamming/hamming.js
@@ -4,6 +4,9 @@
 
 var Hamming = function() {
     this.compute = function(str1, str2) {
+        if (str1.length != str2.length)
+            throw "DNA strands must be of equal length.";
+
         var count = 0;
         for (var i = 0; i < str1.length; i ++) {
             if (str1[i] != str2[i]) count += 1;
