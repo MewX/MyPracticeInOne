fc94ad31e9c81f7a782e7a84d9b392131959b9b4
MewX <imewx@qq.com>
2016-12-29 11:54:59 +1030

    Fixed bugs on js/grains

--- exercism.io/javascript/grains/grains.js
+++ exercism.io/javascript/grains/grains.js
@@ -4,19 +4,25 @@
 var BigInt = require('./big-integer');
 
 function Grains() {
+    sum = new BigInt(1);
     table = initTable();
 
     function initTable() {
         var result = {1: new BigInt(1)};
         for (var i = 2; i <= 64; i++) {
-            result[i] = new BigInt(result[i - 1]).pow(2);
+            result[i] = new BigInt(result[i - 1]).add(new BigInt(result[i - 1]));
+            sum = sum.add(result[i]);
         }
         return result;
     }
 
     this.square = function(i) {
         return table[i].toString();
-    }
+    };
+
+    this.total = function() {
+        return sum.toString();
+    };
 }
 
 module.exports = Grains;
