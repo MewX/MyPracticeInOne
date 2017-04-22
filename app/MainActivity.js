a6a8e85dd1d78bdec2c5b82664ec0cf2303996dc
MewX <imewx@qq.com>
2017-04-23 00:20:37 +0930

    Added filter for tasks

--- app/MainActivity.js
+++ app/MainActivity.js
@@ -20,11 +20,15 @@ const uiTheme = {
 };
 
 class MainActivity extends React.Component {
+    static ACTIVE_ALL = 'ALL';
+    static ACTIVE_PENDING = 'PENDING';
+    static ACTIVE_DONE = 'DONE';
+
     constructor() {
         super();
         this.state = {
-            ds: new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2}),
-            active: 'all',
+            ds: new ListView.DataSource({rowHasChanged: (ori, n) => ori !== n}),
+            active: MainActivity.ACTIVE_ALL,
             edit: '',
             items: [
                 {text: 'Done sample', done: true},
@@ -50,26 +54,37 @@ class MainActivity extends React.Component {
                     <View style={{ backgroundColor: COLOR.green500, height: 24 }} />
                     <Toolbar
                         leftElement="menu"
-                        centerElement="Material Todo List"
+                        centerElement="RN Material Todo List"
                     />
 
                     <TextInput
-                        style={{height: 56, borderColor: 'grey', borderWidth: 0}}
+                        style={{paddingLeft: 12, paddingRight: 12, fontSize: 16, borderColor: 'grey', borderWidth: 0}}
                         onChangeText={(text) => this.setState({edit: text})}
+                        placeholder="What to do?"
                         value={this.state.edit}
                         onSubmitEditing={(event) => this.addNewItem(event.nativeEvent.text)}
                     />
 
                     <ListView
                         style={{ flex: 1 }}
-                        dataSource={this.state.ds.cloneWithRows(this.state.items)}
+                        dataSource={this.state.ds.cloneWithRows(this.state.items.filter((it) => {
+                            switch (this.state.active) {
+                                case MainActivity.ACTIVE_DONE:
+                                    return it.done;
+                                case MainActivity.ACTIVE_PENDING:
+                                    return !it.done;
+                                default:
+                                    return true;
+                            }
+                        }))}
                         renderRow={(rowData) => {
                             // todo: filter items by active
                             let color = rowData.done ? COLOR.grey300 : '#ffffff00';
+                            console.log(rowData.text);
                             return (<TouchableNativeFeedback
                                 background={TouchableNativeFeedback.SelectableBackground()}>
-                                <View style={{alignSelf: "stretch", height: 56, backgroundColor: color}}>
-                                    <Text style={{textAlign: 'center', margin: 30}}>{rowData.title}</Text>
+                                <View style={{alignSelf: "stretch", backgroundColor: color}}>
+                                    <Text style={{padding: 12, textAlign: 'left', justifyContent: 'center', fontSize: 16}}>{rowData.text}</Text>
                                 </View>
                             </TouchableNativeFeedback>);
                         }}
@@ -77,20 +92,20 @@ class MainActivity extends React.Component {
 
                     <BottomNavigation active={this.state.active} hidden={false}>
                         <BottomNavigation.Action
-                            key="all"
+                            key={MainActivity.ACTIVE_ALL}
                             icon="home"
-                            label="all"
-                            onPress={() => this.setState({active: 'all'})}/>
+                            label="All"
+                            onPress={() => this.setState({active: MainActivity.ACTIVE_ALL})}/>
                         <BottomNavigation.Action
-                            key="pending"
+                            key={MainActivity.ACTIVE_PENDING}
                             icon="compare-arrows"
-                            label="pending"
-                            onPress={() => this.setState({active: 'pending'})}/>
+                            label="Pending"
+                            onPress={() => this.setState({active: MainActivity.ACTIVE_PENDING})}/>
                         <BottomNavigation.Action
-                            key="done"
+                            key={MainActivity.ACTIVE_DONE}
                             icon="archive"
-                            label="done"
-                            onPress={() => this.setState({active: 'done'})}/>
+                            label="Done"
+                            onPress={() => this.setState({active: MainActivity.ACTIVE_DONE})}/>
                     </BottomNavigation>
                 </View>
             </ThemeProvider>
@@ -98,9 +113,14 @@ class MainActivity extends React.Component {
     }
 
     addNewItem(str) {
+        // pre process, and ignore empty string
+        str = str.trim();
+        this.setState({edit: ''});
+        if (!str || !str.length) return;
+
         let i = this.state.items;
         i.unshift({text: str, done: false});
-        this.setState({edit: '', items: i});
+        this.setState({items: i});
     }
 }
 
