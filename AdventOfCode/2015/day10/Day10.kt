3ece6d8e4051b814e06035c63c540fd676a2ef41
MewX <imewx@qq.com>
2017-07-10 22:10:27 +0930

    Added day 10 part 1

---
+++ AdventOfCode/2015/day10/Day10.kt
@@ -0,0 +1,41 @@
+import java.util.*
+
+/**
+ * Created by MewX on 7/8/2017.
+ * http://adventofcode.com/2015/day/10
+ *
+ * Part 1 input: 1113222113
+ */
+
+fun main(args: Array<String>) {
+//    val s = Scanner(System.`in`)
+//    var temp = s.nextLine()
+    var temp = args[0]
+    println("Input: $temp")
+//    s.close()
+
+    val sb = StringBuilder()
+    for (times in 1..40) {
+        var prev = temp[0]
+        var idx = 1
+        var counter = 1
+        while (idx < temp.length) {
+            if (temp[idx] == prev) {
+                counter ++
+            } else {
+                sb.append(counter).append(prev)
+                prev = temp[idx]
+                counter = 1
+            }
+
+            idx ++
+        }
+        sb.append(counter).append(prev)
+
+        // move to a new round
+        temp = sb.toString()
+        sb.setLength(0)
+        println(String.format("Round %d: %s (%d)", times, temp, temp.length))
+    }
+
+}
