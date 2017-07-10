415150b9a10309e5cf70ade093ccc3dc58d44ba8
MewX <imewx@qq.com>
2017-07-11 00:38:46 +0930

    WIP: idea of distinguishing array and object

--- AdventOfCode/2015/day12/Day12.kt
+++ AdventOfCode/2015/day12/Day12.kt
@@ -6,13 +6,60 @@ import java.util.*
 
 fun sumLine(str: String): Int = Regex("-?\\d+").findAll(str).sumBy { it.value.toInt() }
 
+fun removeRedObjects(sb: StringBuilder): StringBuilder {
+    var str = sb
+    do {
+        val idx = str.indexOf("\"red\"")
+        if (idx == -1) break
+        println("Found red in: $idx")
+
+        // find beginning index
+        var remaining = 1 // the brace counter
+        var arrayCounter = 1
+        var rmBeg = idx
+        while (rmBeg >= 0 && remaining != 0 && arrayCounter != 0) {
+            rmBeg --
+            when (str[rmBeg]) {
+                '}' -> remaining ++
+                '{' -> remaining --
+                ']' -> arrayCounter ++
+                '[' -> arrayCounter --
+            }
+        }
+        if (arrayCounter == 0) {
+            // array, should not be removed, but for simplifying, still remove the word
+            str = StringBuilder(str.removeRange(idx..idx + 5))
+            continue
+        }
+
+        // find ending index
+        remaining = 1
+        var rmEnd = idx + 4
+        while (rmEnd < str.length && remaining != 0) {
+            rmEnd ++
+            if (str[rmEnd] == '{') remaining ++
+            else if (str[rmEnd] == '}') remaining --
+        }
+        println("Removing: " + str.subSequence(rmBeg..rmEnd))
+        str = StringBuilder(str.removeRange(rmBeg..rmEnd))
+//        println("After removing: " + str)
+    } while (true)
+    return str
+}
+
 fun main(args: Array<String>) {
     val s = Scanner(System.`in`)
-    var sum = 0
+    var sum1 = 0
+    var sum2 = 0
     while (s.hasNextLine()) {
-        sum += sumLine(s.nextLine())
+        val currentLine = s.nextLine()
+        sum1 += sumLine(currentLine)
+
+        val sb = StringBuilder(currentLine)
+        sum2 += sumLine(removeRedObjects(sb).toString())
     }
     s.close()
 
-    println("part 1: $sum")
+    println("part 1: $sum1")
+    println("part 2: $sum2")
 }
