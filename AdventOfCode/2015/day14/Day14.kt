1557f218992a9655557be0e3919596fd08dae3e3
MewX <imewx@qq.com>
2017-07-13 01:28:23 +0930

    Finished part 2 day 14

--- AdventOfCode/2015/day14/Day14.kt
+++ AdventOfCode/2015/day14/Day14.kt
@@ -3,14 +3,15 @@ import java.util.*
 /**
  * Created by MewX on 7/11/2017.
  *
- * Usage: kotlin Day14Kt 2503 < input.txt
+ * Usage:
+ * kotlin Day14Kt 2503 < input.txt
  */
 
-fun calcDistanceAfter(v: Int, tFly: Int, tRest: Int, time: Int): Int {
-    val tRound = tFly + tRest
-    var ret = (time / tRound) * (v * tFly)
-    ret += Math.min(time % tRound, tFly) * v
-    return ret
+data class Info(val name: String, val speed: Int, val tFly: Int, val tRest: Int)
+
+fun calcDistanceAfter(info: Info, time: Int): Int {
+    val tRound = info.tFly + info.tRest
+    return (time / tRound) * (info.speed * info.tFly) + Math.min(time % tRound, info.tFly) * info.speed
 }
 
 fun main(args: Array<String>) {
@@ -18,6 +19,7 @@ fun main(args: Array<String>) {
     val s = Scanner(System.`in`)
 
     var longest = 0
+    val reindeers = mutableMapOf<Info, Int>() // obj, score
     while (s.hasNextLine()) {
         val line = s.nextLine()
 
@@ -26,13 +28,21 @@ fun main(args: Array<String>) {
         val speed=  matches[2].toInt()
         val tFly = matches[3].toInt()
         val tRest = matches[4].toInt()
+        val obj = Info(name, speed, tFly, tRest)
+        reindeers[obj] = 0
 
-        val calc = calcDistanceAfter(speed, tFly, tRest, targetSecond)
+        val calc = calcDistanceAfter(obj, targetSecond)
         println("$name: $calc")
         longest = Math.max(longest, calc)
     }
     s.close()
-
     println("Part 1: $longest")
 
+    // calc part 2
+    for (i in 1..targetSecond) {
+        val scores = reindeers.map { it.key }.map { Pair(it, calcDistanceAfter(it, i)) }
+        val maxScore = scores.map { it.second }.max()
+        scores.filter { it.second == maxScore }.forEach { reindeers.computeIfPresent(it.first) { _, score -> score + 1 } }
+    }
+    println("Part 2: " + reindeers.map { it.value }.max())
 }
