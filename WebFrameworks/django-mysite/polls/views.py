9c3cdac7eaecc4b899b27771bfaaf0eb36d01055
MewX <imewx@qq.com>
2017-11-29 23:21:02 +1030

    Partially finished tutorial 3 of django, because this tuturial change its codes very often

--- WebFrameworks/django-mysite/polls/views.py
+++ WebFrameworks/django-mysite/polls/views.py
@@ -1,6 +1,31 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.shortcuts import render
+from django.template import loader
+
+from .models import Question
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello world")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader .get_template('index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except:
+        raise Http404("question does not exist")
+    return render(request, 'polls/details.html', {'question': question})
+
+
+def results(request, question_id):
+    return HttpResponse("You're looking at the results of question %s." % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
