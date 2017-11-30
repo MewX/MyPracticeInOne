bb60f43b252ca51a1a34b18fcd5c8f68569a94ad
MewX <imewx@qq.com>
2017-12-01 01:18:53 +1030

    Finished Django tutorial 7

--- WebFrameworks/django-mysite/polls/admin.py
+++ WebFrameworks/django-mysite/polls/admin.py
@@ -2,5 +2,21 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
-admin.site.register(Question)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 0
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    # fields = ['pub_date', 'question_text']
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
