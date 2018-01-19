d8003c6cc09e81a52e6477b59180c9218204b667
MewX <imewx@qq.com>
2018-01-20 00:34:38 +1030

    Finished the basic demo same as Django official tutorial in Java Spring

--- WebFrameworks/spring-practice/src/main/java/org/mewx/spring/practice/controller/MainPage.java
+++ WebFrameworks/spring-practice/src/main/java/org/mewx/spring/practice/controller/MainPage.java
@@ -10,10 +10,13 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Comparator;
 import java.util.List;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 @Controller
 public class MainPage {
@@ -38,9 +41,44 @@ public class MainPage {
         }
 
         List<Choice> choices = choiceRepo.findByQuestion(question);
-        model.addAttribute("question_text", question.getQuestionText());
+        choices.sort(Comparator.comparingInt(Choice::getId));
+
+        model.addAttribute("question", question);
         model.addAttribute("choices", choices);
         return "details";
     }
 
+    @RequestMapping(value = "/vote/{id:[\\d]+}.do", method = POST)
+    public String vote(Model model, @PathVariable("id") int questionId, @RequestParam(value = "choice", required = false) Integer choiceId) {
+        Question question = questionRepo.findById(questionId);
+
+        if (choiceId == null) choiceId = -1;
+        Choice choice = choiceRepo.findById(choiceId);
+
+        if (question == null || choice == null || choice.getQuestion().getId() != question.getId()) {
+            model.addAttribute("error_message", "Bad request!");
+            return "details";
+        } else {
+            // TODO: change to atomic method
+            choice.setVotes(choice.getVotes() + 1);
+            choiceRepo.save(choice);
+
+            return "redirect:/result/" + questionId + ".html";
+        }
+    }
+
+    @RequestMapping(value = "/result/{id:[\\d]+}.html", method = GET)
+    public String result(Model model, @PathVariable("id") int id) {
+        Question question = questionRepo.findById(id);
+        if (question == null) {
+            return "redirect:/";
+        }
+
+        List<Choice> choices = choiceRepo.findByQuestion(question);
+        choices.sort(Comparator.comparingInt(Choice::getId));
+
+        model.addAttribute("question", question);
+        model.addAttribute("choices", choices);
+        return "results";
+    }
 }
