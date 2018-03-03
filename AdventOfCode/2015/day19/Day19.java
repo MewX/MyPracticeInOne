e6b47394b54a5b06292dd5c9878e404b64705e72
MewX <imewx@qq.com>
2018-03-03 20:13:22 +1030

    Added 2015 day 19 part 2 solution

--- AdventOfCode/2015/day19/Day19.java
+++ AdventOfCode/2015/day19/Day19.java
@@ -4,11 +4,11 @@ import java.util.Scanner;
 
 public class Day19 {
     private static HashSet<String> set = new HashSet<>();
+    private static ArrayList<String> src = new ArrayList<>(), dst = new ArrayList<>();
+    private static String input = "";
 
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        ArrayList<String> src = new ArrayList<>(), dst = new ArrayList<>();
-        String input = "";
         while (s.hasNextLine()) {
             String temp = s.nextLine().trim();
             int idx = temp.indexOf(" => ");
@@ -26,21 +26,30 @@ public class Day19 {
         s.close();
 
         // calculate
-        for (int idxReplacement = 0; idxReplacement < src.size(); idxReplacement++) {
-            for (int i = 0; i < input.length(); i++) {
-                if (foundEqHere(src.get(idxReplacement), input, i)) {
-                    set.add(input.substring(0, i)
-                            + dst.get(idxReplacement)
-                            + input.substring(i + src.get(idxReplacement).length()));
+        final int PART = 2;
+        if (PART == 1) {
+            for (int idxReplacement = 0; idxReplacement < src.size(); idxReplacement++) {
+                for (int i = 0; i < input.length(); i++) {
+                    if (foundEqHere(src.get(idxReplacement), input, i)) {
+                        set.add(input.substring(0, i)
+                                + dst.get(idxReplacement)
+                                + input.substring(i + src.get(idxReplacement).length()));
+                    }
+
                 }
 
             }
-
+            System.out.println(set.size());
+        } else {
+//            System.out.println(dfs(1, "e", Integer.MAX_VALUE));
+            System.out.println(backDfs(1, input, Integer.MAX_VALUE));
         }
-        System.out.println(set.size());
+
     }
 
     private static boolean foundEqHere(final String key, final String text, final int idx) {
+        if (idx + key.length() > text.length()) return false;
+
         for (int i = 0; i < key.length(); i ++) {
             if (text.charAt(idx + i) != key.charAt(i))
                 return false;
@@ -48,5 +57,80 @@ public class Day19 {
         return true;
     }
 
+    /**
+     * The right logic, but way too slow!
+     *
+     * @param curDepth current depth
+     * @param text     constructed text
+     * @param foundMin the min depth found
+     * @return the min depth
+     */
+    private static int dfs(final int curDepth, final String text, final int foundMin) {
+        if (text.length() > input.length() || curDepth > foundMin) return Integer.MAX_VALUE; // not found
+
+        // src loop
+        int minRet = Integer.MAX_VALUE;
+        for (int i = 0; i < src.size(); i ++) {
+            int loop = text.indexOf(src.get(i));
+            if (loop != -1 && text.substring(0, loop).equals(input.substring(0, loop))) {
+                for (; loop < text.length(); loop++) {
+
+                    if (foundEqHere(src.get(i), text, loop)) {
+                        // build string
+                        final String afterReplace = text.substring(0, loop)
+                                + dst.get(i)
+                                + text.substring(loop + src.get(i).length());
+
+//                        if (set.contains(afterReplace)) continue;
+//                        else set.add(afterReplace);
+
+                        if (afterReplace.equals(input)) {
+                            minRet = Math.min(minRet, curDepth);
+                        } else if (afterReplace.length() <= input.length()) {
+                            System.out.println(afterReplace);
+                            final int tempRet = dfs(curDepth + 1, afterReplace, minRet);
+                            minRet = Math.min(minRet, tempRet);
+                        }
+
+                    }
+                }
+            }
+        }
+        return minRet;
+    }
+
+
+    private static int backDfs(final int curDepth, final String text, final int foundMin) {
+        if (text.length() > input.length() || curDepth > foundMin) return Integer.MAX_VALUE; // not found
+
+        int minRet = Integer.MAX_VALUE;
+        for (int idxDst = 0; idxDst < dst.size(); idxDst++) {
+            int idxLoop = text.indexOf(dst.get(idxDst));
+            while (idxLoop != -1) {
+                final String afterReplace = text.substring(0, idxLoop)
+                        + src.get(idxDst)
+                        + text.substring(idxLoop + dst.get(idxDst).length());
+
+                if (!set.contains(afterReplace)) {
+                    set.add(afterReplace);
+
+                    if (afterReplace.equals("e")) {
+                        minRet = Math.min(minRet, curDepth);
+                        System.out.println(minRet);
+                        System.exit(0);
+                    } else {
+//                        System.out.println(afterReplace);
+                        final int tempRet = backDfs(curDepth + 1, afterReplace, minRet);
+                        minRet = Math.min(minRet, tempRet);
+                    }
+
+                }
+
+                // next loop
+                idxLoop = text.indexOf(dst.get(idxDst), idxLoop + 1);
+            }
+        }
+        return minRet;
+    }
 
 }
