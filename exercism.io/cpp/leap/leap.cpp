3eedfd12a2e94471d9416a902b21690c3ab17b8d
MewX <xiayuanzhong@gmail.com>
2020-04-30 05:15:22 +0000

    Fixed solutions for cpp/leap based on comments on exercism.io

--- exercism.io/cpp/leap/leap.cpp
+++ exercism.io/cpp/leap/leap.cpp
@@ -2,10 +2,6 @@
 
 namespace leap {
 	bool is_leap_year(int year) {
-		if (year <= 0) {
-			return false;
-		}
-
 		return year % 400 == 0 || (year % 4 ==0 && year % 100 != 0);
 	}
 }  // namespace leap
