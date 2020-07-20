9dbd771d3de174759156509fc7cb61d59f5056e4
MewX <xiayuanzhong@gmail.com>
2020-07-20 01:25:28 +0000

    Added solution for go/luhn on exercism.io

---
+++ exercism.io/go/luhn/luhn.go
@@ -0,0 +1,33 @@
+package luhn
+
+// Valid tells if the input string follows the Luhn formula.
+func Valid(str string) bool {
+	sum, count := 0, 0
+	secondNumberFlipper := false
+	for i := len(str) - 1; i >= 0; i-- {
+		// Can be space.
+		if str[i] == ' ' {
+			continue
+		}
+
+		// Returning false if containing non-digits.
+		if str[i] < '0' || '9' < str[i] {
+			return false
+		}
+
+		num := int(str[i] - '0')
+		if secondNumberFlipper {
+			// Double every second digit.
+			num += num
+			if num > 9 {
+				num -= 9
+			}
+		}
+		sum += num
+		count++
+
+		// Flip the flipper.
+		secondNumberFlipper = !secondNumberFlipper
+	}
+	return count > 1 && sum%10 == 0
+}
