286a8cfc8374d1776da12d165d277b68cdebf611
MewX <xiayuanzhong@gmail.com>
2020-09-16 11:07:51 +0000

    Added solution to exercism.io/dart/resistor_color

---
+++ exercism.io/dart/resistor-color/lib/resistor_color.dart
@@ -0,0 +1,16 @@
+class ResistorColor {
+  final List<String> colors = [
+    'black',
+    'brown',
+    'red',
+    'orange',
+    'yellow',
+    'green',
+    'blue',
+    'violet',
+    'grey',
+    'white',
+  ];
+
+  int colorCode(String color) => colors.indexOf(color);
+}
