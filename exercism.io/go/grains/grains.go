580c9e12501677f09455705bbca5c492451bbf58
MewX <xiayuanzhong+github@gmail.com>
2020-10-15 11:56:54 +1100

    Updated solution for exercism.io/go/grains further

--- exercism.io/go/grains/grains.go
+++ exercism.io/go/grains/grains.go
@@ -7,11 +7,16 @@ func Square(n int) (uint64, error) {
 	if n <= 0 || n > 64 {
 		return 0, errors.New("square id must be between 1 - 64")
 	}
-	return uint64(1) << (n - 1), nil
+	return 1 << (n - 1), nil
 }
 
 // Total returns the total number of grains in the chess board.
 func Total() uint64 {
 	// Every bit is set to 1 for a uint64.
-	return ^uint64(0)
+	// return ^uint64(0)
+
+	// Or, using 2^63 - 1. This operation won't trigger overflow error as it is
+	// evaluated at compile time as constant literal value.
+	// https://golang.org/ref/spec#Constant_expressions
+	return 1<<64 - 1
 }
