70ea016e145c28cb7ba7afe9baa1218f693a274b
MewX <xiayuanzhong+github@gmail.com>
2020-10-17 12:05:30 +1100

    Added solution to aoc/2016/day05 part 2

--- AdventOfCode/2016/day05/Day05.go
+++ AdventOfCode/2016/day05/Day05.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"bufio"
-	"os"
-	"io"
-	"fmt"
 	"crypto/md5"
+	"fmt"
+	"io"
+	"os"
+	"strings"
 )
 
 func main() {
@@ -13,21 +14,51 @@ func main() {
 	line, _, _ := reader.ReadLine()
 	str := string(line)
 
+	// Part 1
+	// fmt.Println(findPart1(str))
+
+	// Part 2
+	fmt.Println(findPart2(str))
+}
+
+func calcDigest(str string) string {
+	h := md5.New()
+	io.WriteString(h, str)
+	return fmt.Sprintf("%x", h.Sum(nil))
+}
+
+func findPart1(str string) string {
 	result := ""
 	index := uint64(0)
 	for len(result) < 8 {
 		v := str + fmt.Sprint(index)
-
-		// Calculate MD5.
-		h := md5.New()
-		io.WriteString(h, v)
-		digest := fmt.Sprintf("%x", h.Sum(nil))
+		digest := calcDigest(v)
 		// fmt.Printf("%s - %s", v, digest)
 
 		if digest[:5] == "00000" {
 			result += string(digest[5])
-		} 
+		}
 		index++
 	}
-	fmt.Println(result)
+	return result
+}
+
+func findPart2(str string) string {
+	result := "--------"
+	index := uint64(0)
+	for strings.Index(result, "-") >= 0 {
+		v := str + fmt.Sprint(index)
+		digest := calcDigest(v)
+
+		if digest[:5] == "00000" {
+			i := int(digest[5]) - '0'
+			// fmt.Printf("%s - %d\n", digest, i)
+			if 0 <= i && i < 8 && result[i] == '-' {
+				result = result[:i] + string(digest[6]) + result[i+1:]
+				fmt.Println(result)
+			}
+		}
+		index++
+	}
+	return result
 }
