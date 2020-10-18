69524a18732458a33f790f39f09895493f2fcbc8
MewX <xiayuanzhong+github@gmail.com>
2020-10-18 12:21:43 +1100

    Updated solution for exercism/go/parallel_letter_frequency further

--- exercism.io/go/parallel-letter-frequency/parallel_letter_frequency.go
+++ exercism.io/go/parallel-letter-frequency/parallel_letter_frequency.go
@@ -13,24 +13,21 @@ func Frequency(s string) FreqMap {
 	return m
 }
 
-// freqencyWorker calculates rune frequency in a single string, and update the
-// shared frequency map.
-func freqencyWorker(s string, c chan FreqMap) {
-	c <- Frequency(s)
-}
-
 // ConcurrentFrequency calculates the rune frequency from all strings
 // concurrently.
 func ConcurrentFrequency(strs []string) FreqMap {
-	c := make(chan FreqMap, len(strs))
-	for _, str := range strs {
-		go freqencyWorker(str, c)
+	c := make(chan *FreqMap, 10)
+	for i := range strs {
+		go func(index int) {
+			m := Frequency(strs[index])
+			c <- &m
+		}(i)
 	}
 
 	// Merge all maps.
 	m := FreqMap{}
 	for range strs {
-		for k, v := range <-c {
+		for k, v := range *(<-c) {
 			m[k] += v
 		}
 	}
