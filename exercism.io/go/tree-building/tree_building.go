4d4913162e6aefa0dc6aaa6039345e43ef954fda
MewX <xiayuanzhong+github@gmail.com>
2020-10-20 22:00:41 +1100

    Updated solution for exercism.io/go/tree_building further

--- exercism.io/go/tree-building/tree_building.go
+++ exercism.io/go/tree-building/tree_building.go
@@ -20,31 +20,23 @@ type Node struct {
 // AppendChild appends another node to the Children list.
 func (n *Node) AppendChild(node *Node) {
 	n.Children = append(n.Children, node)
-	sort.Slice(n.Children, func(i, j int) bool {
-		return n.Children[i].ID < n.Children[j].ID
-	})
 }
 
 // Build constructs the tree.
 func Build(records []Record) (*Node, error) {
-	// Adding 1 on size to avoid out of index when returning nodeList[0].
-	nodeList := make([]*Node, len(records)+1)
-	for _, r := range records {
-		if r.ID < 0 || r.ID >= len(records) || nodeList[r.ID] != nil ||
-			r.ID < r.Parent || r.ID == r.Parent && r.ID != 0 {
-			return nil, errors.New("invalid node ID")
+	nodeList := make(map[int]*Node)
+	sort.SliceStable(records, func(i, j int) bool {
+		return records[i].ID < records[j].ID
+	})
+
+	for i, r := range records {
+		if r.ID != i || r.ID < r.Parent || r.ID == r.Parent && r.ID != 0 {
+			return nil, errors.New("invalid node ID or parent ID")
 		}
 		nodeList[r.ID] = &Node{
 			ID: r.ID,
 		}
-	}
-
-	// Now, all r.ID are guaranteed to be valid.
-	for _, r := range records {
-		if r.Parent < 0 || r.Parent >= len(records) ||
-			nodeList[r.Parent] == nil {
-			return nil, errors.New("invalid parent ID")
-		} else if r.ID != 0 {
+		if i != 0 {
 			nodeList[r.Parent].AppendChild(nodeList[r.ID])
 		}
 	}
