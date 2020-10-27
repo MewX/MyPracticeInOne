2fb428e20a9315c83f2c71000dc9c020d1a9da45
MewX <xiayuanzhong@gmail.com>
2020-10-27 02:28:52 +0000

    Updated solution for exercism.io/go/tournament further

--- exercism.io/go/tournament/tournament.go
+++ exercism.io/go/tournament/tournament.go
@@ -17,53 +17,10 @@ type team struct {
 	points      int
 }
 
-func addMatch(a team, b team, result string) (team, team, error) {
-	var e error = nil
-	a.matchPlayed++
-	b.matchPlayed++
-	switch result {
-	case "win":
-		a.win++
-		b.loss++
-		a.points += 3
-	case "draw":
-		a.draw++
-		b.draw++
-		a.points++
-		b.points++
-	case "loss":
-		a.loss++
-		b.win++
-		b.points += 3
-	default:
-		e = errors.New("unknown match result")
-	}
-	return a, b, e
-}
-
-func writeMatchResults(teamMap map[string]team, w io.Writer) {
-	names := make([]string, 0, len(teamMap))
-	for key := range teamMap {
-		names = append(names, key)
-	}
-
-	sort.Slice(names, func(i, j int) bool {
-		ti, tj := teamMap[names[i]], teamMap[names[j]]
-		return ti.points > tj.points ||
-			ti.points == tj.points && names[i] < names[j]
-	})
-
-	fmt.Fprintf(w, "Team                           | MP |  W |  D |  L |  P\n")
-	for _, name := range names {
-		t := teamMap[name]
-		fmt.Fprintf(w, "%-30s | %2d | %2d | %2d | %2d | %2d\n",
-			name, t.matchPlayed, t.win, t.draw, t.loss, t.points)
-	}
-}
-
 // Tally calculates the competition results.
 func Tally(reader io.Reader, writer io.Writer) error {
-	teamMap := map[string]team{}
+	// Read each line from the reader and parse them into 3-token slices.
+	lines := [][]string{}
 	scanner := bufio.NewScanner(reader)
 	for scanner.Scan() {
 		line := scanner.Text()
@@ -73,17 +30,53 @@ func Tally(reader io.Reader, writer io.Writer) error {
 
 		secs := strings.Split(line, ";")
 		if len(secs) != 3 {
-			return errors.New("input format wrong")
+			return errors.New("input format wrong for line: " + line)
 		}
-
-		var err error
-		teamMap[secs[0]], teamMap[secs[1]], err =
-			addMatch(teamMap[secs[0]], teamMap[secs[1]], secs[2])
-		if err != nil {
-			return err
-		}
+		lines = append(lines, secs)
 	}
 
-	writeMatchResults(teamMap, writer)
+	// Update team match based on the inputs.
+	teamMap := map[string]team{}
+	for _, sections := range lines {
+		a, b := teamMap[sections[0]], teamMap[sections[1]]
+		a.matchPlayed++
+		b.matchPlayed++
+		switch sections[2] {
+		case "win":
+			a.win++
+			b.loss++
+			a.points += 3
+		case "draw":
+			a.draw++
+			b.draw++
+			a.points++
+			b.points++
+		case "loss":
+			a.loss++
+			b.win++
+			b.points += 3
+		default:
+			return errors.New("unknown match result: " + sections[2])
+		}
+		teamMap[sections[0]], teamMap[sections[1]] = a, b
+	}
+
+	// Write match results.
+	names := make([]string, 0, len(teamMap))
+	for key := range teamMap {
+		names = append(names, key)
+	}
+
+	sort.Slice(names, func(i, j int) bool {
+		pti, ptj := teamMap[names[i]].points, teamMap[names[j]].points
+		return pti > ptj || pti == ptj && names[i] < names[j]
+	})
+
+	fmt.Fprintf(writer, "%-30s | MP |  W |  D |  L |  P\n", "Team")
+	for _, name := range names {
+		t := teamMap[name]
+		fmt.Fprintf(writer, "%-30s | %2d | %2d | %2d | %2d | %2d\n",
+			name, t.matchPlayed, t.win, t.draw, t.loss, t.points)
+	}
 	return nil
 }
