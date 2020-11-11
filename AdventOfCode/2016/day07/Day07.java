938b971056ce794e022fe1032da58d4302e8128d
MewX <axx+github@google.com>
2020-11-11 22:05:30 +1100

    Added solution for aoc 2016 day07 part 2

--- AdventOfCode/2016/day07/Day07.java
+++ AdventOfCode/2016/day07/Day07.java
@@ -22,6 +22,15 @@ public class Day07 {
             }
         }
         System.out.println("Part 1: " + count1);
+
+        // Part 2.
+        int count2 = 0;
+        for (String input : inputs) {
+            if (supportSSL(input)) {
+                count2++;
+            }
+        }
+        System.out.println("Part 2: " + count2);
     }
 
     private static boolean supportTLS(String input) {
@@ -56,5 +65,45 @@ public class Day07 {
             }
         }
         return false;
+    }
+
+    private static boolean supportSSL(String input) {
+        List<String> babs = new ArrayList<>();
+        List<String> ips = new ArrayList<>();
+        boolean in = false;
+        int beg = 0;
+        for (int i = 0; i < input.length(); i++) {
+            if (!in && input.charAt(i) == '[') {
+                in = true;
+                babs.addAll(findWantedThreeLetters(input.substring(beg, i)));
+                beg = i + 1;
+            } else if (in && input.charAt(i) == ']') {
+                in = false;
+                ips.add(input.substring(beg, i));
+                beg = i + 1;
+            }
+        }
+        // Last one.
+        babs.addAll(findWantedThreeLetters(input.substring(beg)));
+
+        // Check each ips.
+        for (String ip : ips) {
+            for (String bab : babs) {
+                if (ip.contains(bab)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private static List<String> findWantedThreeLetters(String str) {
+        List<String> ret = new ArrayList<>();
+        for (int i = 0; i < str.length() - 2; i++) {
+            if (str.charAt(i) == str.charAt(i + 2) && str.charAt(i) != str.charAt(i + 1)) {
+                ret.add("" + str.charAt(i + 1) + str.charAt(i) + str.charAt(i + 1));
+            }
+        }
+        return ret;
     }
 }
