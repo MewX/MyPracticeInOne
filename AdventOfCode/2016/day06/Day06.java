1f4eea4945192ebd11e5f70924b37004cd56749d
MewX <axx+github@google.com>
2020-11-11 20:31:14 +1100

    Added solution for aoc 2016 day06 part 2

--- AdventOfCode/2016/day06/Day06.java
+++ AdventOfCode/2016/day06/Day06.java
@@ -15,12 +15,24 @@ class Day06 {
         s.close();
 
         // Part 1.
+        System.out.print("Part 1: ");
         for (int i = 0; i < inputs.get(0).length(); i++) {
             char[] chars = new char[inputs.size()];
             for (int j = 0; j < inputs.size(); j++) {
                 chars[j] = inputs.get(j).charAt(i);
             }
             System.out.print(highestFrequency(chars));
+        }
+        System.out.println();
+
+        // Part 2.
+        System.out.print("Part 2: ");
+        for (int i = 0; i < inputs.get(0).length(); i++) {
+            char[] chars = new char[inputs.size()];
+            for (int j = 0; j < inputs.size(); j++) {
+                chars[j] = inputs.get(j).charAt(i);
+            }
+            System.out.print(leastFrequency(chars));
         }
         System.out.println();
     }
@@ -49,5 +61,31 @@ class Day06 {
             high = tempChar;
         }
         return high;
+    }
+
+    public static char leastFrequency(char[] chars) {
+        // Ascending.
+        Arrays.sort(chars);
+
+        char low = chars[0], tempChar = low;
+        int freq = Integer.MAX_VALUE, tempFreq = 1;
+        for (int i = 1; i < chars.length; i++) {
+            if (chars[i] == tempChar) {
+                tempFreq ++;
+            } else {
+                if (tempFreq < freq) {
+                    low = tempChar;
+                    freq = tempFreq;
+                }
+
+                // Next possible one.
+                tempChar = chars[i];
+                tempFreq = 1;
+            }
+        }
+        if (tempFreq < freq) {
+            low = tempChar;
+        }
+        return low;
     }
 }
