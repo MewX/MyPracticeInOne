270aa74f08961d3b9983e59a954787b0b1c09691
MewX <axx+github@google.com>
2020-12-01 16:14:50 +1100

    Added aoc/2020/day01 part 2

--- AdventOfCode/2020/day01/Day01.java
+++ AdventOfCode/2020/day01/Day01.java
@@ -1,21 +1,29 @@
-import java.util.HashSet;
-import java.util.Scanner;
-import java.util.Set;
+import java.util.*;
 
 public class Day01 {
     public static void main(String[] args) {
         Set<Integer> set = new HashSet<>();
+        List<Integer> list = new ArrayList<>();
 
         Scanner s = new Scanner(System.in);
         while (s.hasNextInt()) {
             int ex = s.nextInt();
-            System.out.println(2020 - ex);
+            list.add(ex);
             if (set.contains(ex)) {
                 System.out.println("part 1: " + (2020 - ex) * ex);
-                break;
             }
             set.add(2020 - ex);
         }
-    }
 
+        // Part 2, stupid brute force.
+        for (int i = 0; i < list.size(); i++) {
+            for (int j = i + 1; j < list.size(); j++) {
+                for (int k = j + 1; k < list.size(); k++) {
+                    if (list.get(i) + list.get(j) + list.get(k) == 2020) {
+                        System.out.println("part 2: " + list.get(i) * list.get(j) * list.get(k));
+                    }
+                }
+            }
+        }
+    }
 }
