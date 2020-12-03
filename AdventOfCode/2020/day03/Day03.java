75be590bbda066370b084298eed6b14996d2e1c9
MewX <axx+github@google.com>
2020-12-03 16:21:42 +1100

    Added aoc/2020/day03 part 2

--- AdventOfCode/2020/day03/Day03.java
+++ AdventOfCode/2020/day03/Day03.java
@@ -11,7 +11,18 @@ public class Day03 {
             area.add(new StringBuilder(line));
         }
 
-        int right = 3, down = 1;
+        int count1 = countTrees(area, 3, 1);
+        System.out.println("part 1: " + count1);
+
+        long count2 = (long)count1 *
+                countTrees(area, 1, 1) *
+                countTrees(area, 5, 1) *
+                countTrees(area, 7, 1) *
+                countTrees(area, 1, 2);
+        System.out.println("part 2: " + count2);
+    }
+
+    static int countTrees(List<StringBuilder> area, int right, int down) {
         int x = right, y = down;
         int count = 0;
         while (y < area.size()) {
@@ -27,8 +38,7 @@ public class Day03 {
             x += right;
             y += down;
         }
-        System.out.println("part 1: " + count);
-
+        return count;
     }
 
 }
