ef211d22bf5ee24cdee21090524b3189f97c896e
MewX <axx+github@google.com>
2020-12-06 16:11:20 +1100

    Added aoc/2020/day06 part 2

--- AdventOfCode/2020/day06/Day06.java
+++ AdventOfCode/2020/day06/Day06.java
@@ -4,6 +4,7 @@ public class Day06 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         List<Set<Character>> groups = new ArrayList<>();
+        List<Set<Character>> groups2 = new ArrayList<>();
         groups.add(new HashSet<>());
         while (s.hasNextLine()) {
             String line = s.nextLine().trim();
@@ -12,11 +13,20 @@ public class Day06 {
                 continue;
             }
 
-            Set<Character> g = groups.get(groups.size() - 1);
+            Set<Character> g = new HashSet<>();
             for (int i = 0; i < line.length(); i++) {
                 char c = line.charAt(i);
                 g.add(c);
             }
+
+            // part 1
+            groups.get(groups.size() - 1).addAll(g);
+
+            // part 2
+            if (groups2.size() != groups.size()) {
+                groups2.add(new HashSet<>(g));
+            }
+            groups2.get(groups2.size() - 1).retainAll(g);
         }
 
         int count = 0;
@@ -24,5 +34,11 @@ public class Day06 {
             count += g.size();
         }
         System.out.println("part 1: " + count);
+
+        count = 0;
+        for (Set<Character> g : groups2) {
+            count += g.size();
+        }
+        System.out.println("part 2: " + count);
     }
 }
