d52f8729f475f15f28ea1cc4362c35a01df1d811
MewX <axx+github@google.com>
2020-12-07 16:18:52 +1100

    Added aoc/2020/day07 part 1

---
+++ AdventOfCode/2020/day07/Day07.java
@@ -0,0 +1,84 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Day07 {
+    public static final String SEP = "bags contain";
+
+    static class Bag {
+        String color;
+        Map<Bag, Integer> contains;
+
+        Bag(String color) {
+            this.color = color;
+            contains = new HashMap<>();
+        }
+    }
+
+    public static void main(String[] args) {
+        Map<String, Bag> bagMap = new HashMap<>();
+
+        Scanner s = new Scanner(System.in);
+        while (s.hasNextLine()) {
+            String line = s.nextLine().trim();
+            if (line.isEmpty()) {
+                continue;
+            }
+
+            String color = line.substring(0, line.indexOf(SEP)).trim();
+            String[] bagNames = line.substring(line.indexOf(SEP) + SEP.length()).trim().split(",");
+            for (String name : bagNames) {
+                // Current color.
+                if (!bagMap.containsKey(color)) {
+                    Bag b = new Bag(color);
+                    bagMap.put(color, b);
+                }
+
+                name = name.trim();
+                String numString = name.substring(0, name.indexOf(" ")).trim();
+                if (numString.equals("no")) {
+                    continue;
+                }
+
+                int num = Integer.parseInt(numString);
+                String cc = name.substring(name.indexOf(" "), name.lastIndexOf(" ")).trim();
+
+                if (!bagMap.containsKey(cc)) {
+                    Bag b = new Bag(cc);
+                    bagMap.put(cc, b);
+                }
+
+                Bag b = bagMap.get(color);
+                b.contains.put(bagMap.get(cc), num);
+            }
+        }
+
+        // DFS
+        int count = 0;
+        String target = "shiny gold";
+        for (Bag b : bagMap.values()) {
+            if (!b.color.equals(target) && dfs(b, target)) {
+                count ++;
+            }
+        }
+        System.out.println("part 1: " + count);
+    }
+
+    static boolean dfs(Bag bag, String target) {
+        if (bag.color.equals(target)) {
+            return true;
+        }
+
+        if (bag.contains.size() == 0) {
+            return false;
+        }
+
+        for (Bag b : bag.contains.keySet()) {
+            if (dfs(b, target)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
