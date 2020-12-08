cdb86890fee6676bddc7e3c9b3bb4546c125d4d1
MewX <axx+github@google.com>
2020-12-08 15:47:23 +1100

    Added java template for aoc

---
+++ AdventOfCode/tmpl/JavaTmpl.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class JavaTmpl {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        while (s.hasNextLine()) {
+            String line = s.nextLine().trim();
+
+        }
+
+        System.out.println("part 1: ");
+    }
+}
