67422a7dbe18aab8c85ccba65da372637c712b95
MewX <axx+github@google.com>
2020-12-15 16:27:08 +1100

    Added aoc/2020/day15 part 2

--- AdventOfCode/2020/day15/Day15.java
+++ AdventOfCode/2020/day15/Day15.java
@@ -18,20 +18,30 @@ public class Day15 {
         s.close();
 
         // Part 1.
-        final int target = 2020;
+        System.out.println("part 1: " + calc(list, 2020));
+
+        // Part 2.
+        System.out.println("part 2: " + calc(list, 30000000));
+    }
+
+    static int calc(List<Integer> list, int target) {
+        Map<Integer, Integer> indexMap = new HashMap<>();
+
+        // Init map.
+        for (int i = 0; i < list.size() - 1; i++) {
+            indexMap.put(list.get(i), i);
+        }
+
         while (list.size() < target) {
-            // Update the list.
             final int last = list.get(list.size() - 1);
-            final List<Integer> sublist = list.subList(0, list.size() - 1);
-            if (sublist.contains(last)) {
-                int lastTurn = list.size();
-                int secondLastTurn = sublist.lastIndexOf(last) + 1;
-                list.add(lastTurn - secondLastTurn);
+            if (indexMap.containsKey(last)) {
+                list.add(list.size() - indexMap.get(last) - 1);
             } else {
                 list.add(0);
             }
-            System.out.format("%d: %d\n", list.size(), list.get(list.size() - 1));
+            indexMap.put(list.get(list.size() - 2), list.size() - 2);
+//            System.out.format("%d: %d\n", list.size(), list.get(list.size() - 1));
         }
-        System.out.println("part 1: " + list.get(target - 1));
+        return list.get(list.size() - 1);
     }
 }
