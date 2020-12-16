832002f6972916395d29d515abe4481b9c31ba61
MewX <axx+github@google.com>
2020-12-16 17:44:38 +1100

    Updated aoc/2020/day16 part 2

--- AdventOfCode/2020/day16/Day16.java
+++ AdventOfCode/2020/day16/Day16.java
@@ -3,7 +3,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Day16 {
-    private static Pattern info = Pattern.compile("(.+?): (\\d+?)-(\\d+?) or (\\d+?)-(\\d+?)$");
+    private static final Pattern info = Pattern.compile("(.+?): (\\d+?)-(\\d+?) or (\\d+?)-(\\d+?)$");
 
     public static void main(String[] args) {
         List<Integer> myTickets = new ArrayList<>();
@@ -30,7 +30,6 @@ public class Day16 {
                 case 1:
                     // Ues the pattern.
                     Matcher m = info.matcher(line);
-                    System.out.println("matching: " + line);
                     if (m.find()) {
                         fields.put(m.group(1), new HashSet<>());
                         for (int i = Integer.parseInt(m.group(2)); i <= Integer.parseInt(m.group(3)); i++) {
@@ -39,8 +38,6 @@ public class Day16 {
                         for (int i = Integer.parseInt(m.group(4)); i <= Integer.parseInt(m.group(5)); i++) {
                             fields.get(m.group(1)).add(i);
                         }
-                    } else {
-                        assert false;
                     }
                     break;
                 case 2:
@@ -67,20 +64,16 @@ public class Day16 {
         List<List<Integer>> validTickets = new ArrayList<>();
         for (List<Integer> row : nearbyTickets) {
             boolean ticketValid = true;
-            for (Integer t : row) {
-                boolean matched = false;
+            elemLoop: for (Integer t : row) {
                 for (String key : fields.keySet()) {
                     if (fields.get(key).contains(t)) {
-                        matched = true;
-                        break;
+                        continue elemLoop;
                     }
                 }
 
-                if (!matched) {
-                    System.out.println("not found " + t);
-                    count += t;
-                    ticketValid = false;
-                }
+                // Not found.
+                count += t;
+                ticketValid = false;
             }
 
             if (ticketValid) {
@@ -107,39 +100,22 @@ public class Day16 {
                 }
                 possibleMapping.get(i).removeAll(removed);
             }
-
-            System.out.format("index %d: ", i);
-            for (String key : possibleMapping.get(i)) {
-                System.out.print(key + ", ");
-            }
-            System.out.println();
         }
 
         // Eliminations.
         String[] order = new String[fields.keySet().size()];
-        out: while (true) {
+        while (remainingMap(possibleMapping)) {
             for (int i = 0; i < possibleMapping.size(); i ++) {
-                if (possibleMapping.get(i).size() == 1) {
-                    order[i] = possibleMapping.get(i).iterator().next();
-
-                    // Start eliminations.
-                    for (Set<String> mapping : possibleMapping) {
-                        mapping.remove(order[i]);
-                    }
-                    continue out;
+                if (possibleMapping.get(i).size() != 1) {
+                    continue;
                 }
-            }
 
-            // Check exist condition.
-            boolean canExit = true;
-            for (Set<String> mapping : possibleMapping) {
-                if (mapping.size() != 0) {
-                    canExit = false;
-                    break;
+                order[i] = possibleMapping.get(i).iterator().next();
+
+                // Start eliminations.
+                for (Set<String> mapping : possibleMapping) {
+                    mapping.remove(order[i]);
                 }
-            }
-            if (canExit) {
-                break;
             }
         }
 
@@ -153,15 +129,12 @@ public class Day16 {
         System.out.println("part 2: " + product);
     }
 
-    static boolean validBuild(Map<String, Set<Integer>> fieldMap, List<String> order, List<List<Integer>> validTickets) {
-        for (int i = 0; i < order.size(); i++) {
-            Set<Integer> ranges = fieldMap.get(order.get(i));
-            for (List<Integer> row : validTickets) {
-                if (!ranges.contains(row.get(i))) {
-                    return false;
-                }
+    static boolean remainingMap(List<Set<String>> possibleMapping) {
+        for (Set<String> mapping : possibleMapping) {
+            if (mapping.size() != 0) {
+                return true;
             }
         }
-        return true;
+        return false;
     }
 }
