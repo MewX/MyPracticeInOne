4896784ff0e100f7c3ba663be23bf282a232e0b9
MewX <axx+github@google.com>
2020-12-17 17:14:24 +1100

    Added aoc/2020/day17 part 2

--- AdventOfCode/2020/day17/Day17.java
+++ AdventOfCode/2020/day17/Day17.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 
 public class Day17 {
     public static void main(String[] args) {
-        List<List<List<Character>>> space = new ArrayList<>();
-        space.add(new ArrayList<>());
+        List<List<List<List<Character>>>> space = new ArrayList<>();
+        space.add(new ArrayList<>(new ArrayList<>()));
 
         Scanner s = new Scanner(System.in);
         while (s.hasNextLine()) {
@@ -16,28 +16,32 @@ public class Day17 {
 
             // # is active; . is inactive.
             List<Character> l = new ArrayList<>();
-            space.get(space.size() - 1).add(l);
+            space.get(0).get(0).add(l);
             for (int i = 0; i < line.length(); i++) {
                 l.add(line.charAt(i));
             }
         }
         s.close();
 
-        // Part 1.
-        List<List<List<Character>>> temp = space;
+        // Part 1 omitted (see git history).
+
+        // Part 2.
+        List<List<List<List<Character>>>> temp = space;
         for (int i = 0; i < 6; i++) {
             temp = nextCycle(temp);
             System.out.format("Cycle #%d: %d\n", i + 1, countActive(temp));
         }
     }
 
-    static int countActive(List<List<List<Character>>> space) {
+    static int countActive(List<List<List<List<Character>>>> space) {
         int count = 0;
-        for (List<List<Character>> layer : space) {
-            for (List<Character> row : layer) {
-                for (Character c : row) {
-                    if (c == '#') {
-                        count ++;
+        for (List<List<List<Character>>> layer : space) {
+            for (List<List<Character>> row : layer) {
+                for (List<Character> www : row) {
+                    for (Character c : www) {
+                        if (c == '#') {
+                            count ++;
+                        }
                     }
                 }
             }
@@ -45,40 +49,49 @@ public class Day17 {
         return count;
     }
 
-    static List<List<List<Character>>> nextCycle(List<List<List<Character>>> space) {
+    static List<List<List<List<Character>>>> nextCycle(List<List<List<List<Character>>>> space) {
         // Make a copy.
-        List<List<List<Character>>> ns = copy(space);
+        List<List<List<List<Character>>>> ns = copy(space);
 
-        // Add one empty layer.
-        ns.add(0, createEmptyLayer(ns.get(0).size() + 2, ns.get(0).get(0).size() + 2));
-        ns.add(createEmptyLayer(ns.get(0).size() + 2, ns.get(0).get(0).size() + 2));
+        // Add two empty layers.
+        ns.add(0, createEmptyLayer(ns.get(0).size() + 2, ns.get(0).get(0).size() + 2, ns.get(0).get(0).get(0).size() + 2));
+        ns.add(createEmptyLayer(ns.get(0).size() + 2, ns.get(0).get(0).size() + 2, ns.get(0).get(0).get(0).size() + 2));
         for (int z = 1; z < ns.size() - 1; z++) {
             // Two rows.
-            ns.get(z).add(0, createEmptyRow(ns.get(0).get(0).size()));
-            ns.get(z).add(createEmptyRow(ns.get(0).get(0).size()));
+            ns.get(z).add(0, createEmptyRow(ns.get(0).get(0).size(), ns.get(0).get(0).get(0).size()));
+            ns.get(z).add(createEmptyRow(ns.get(0).get(0).size(), ns.get(0).get(0).get(0).size()));
 
             for (int x = 1; x < ns.get(z).size() - 1; x++) {
-                // Two chars.
-                ns.get(z).get(x).add(0, '.');
-                ns.get(z).get(x).add('.');
+                // Two WWW.
+                ns.get(z).get(x).add(0, createEmptyWWW(ns.get(0).get(0).get(0).size()));
+                ns.get(z).get(x).add(createEmptyWWW(ns.get(0).get(0).get(0).size()));
+
+                for (int y = 1; y < ns.get(z).get(x).size() - 1; y++) {
+                    // Two chars.
+                    ns.get(z).get(x).get(y).add(0, '.');
+                    ns.get(z).get(x).get(y).add('.');
+                }
             }
         }
 
         // Make change.
-        List<List<List<Character>>> nsCopy = copy(ns);
+        List<List<List<List<Character>>>> nsCopy = copy(ns);
         for (int z = 0; z < ns.size(); z++) {
-            List<List<Character>> layer = ns.get(z);
+            List<List<List<Character>>> layer = ns.get(z);
             for (int x = 0; x < layer.size(); x++) {
-                List<Character> row = layer.get(x);
+                List<List<Character>> row = layer.get(x);
                 for (int y = 0; y < row.size(); y++) {
-                    int c = countNeighbour(nsCopy, x, y, z);
-                    boolean selfActive = checkActive(nsCopy, x, y, z);
+                    List<Character> www = row.get(y);
+                    for (int w = 0; w < www.size(); w++) {
+                        int c = countNeighbour(nsCopy, x, y, z, w);
+                        boolean selfActive = checkActive(nsCopy, x, y, z, w);
 
-                    if (selfActive && !(c == 2 || c == 3)) {
-                        ns.get(z).get(x).set(y, '.');
-                    } else if (!selfActive && c == 3) {
-                        // Make active.
-                        ns.get(z).get(x).set(y, '#');
+                        if (selfActive && !(c == 2 || c == 3)) {
+                            ns.get(z).get(x).get(y).set(w, '.');
+                        } else if (!selfActive && c == 3) {
+                            // Make active.
+                            ns.get(z).get(x).get(y).set(w, '#');
+                        }
                     }
                 }
             }
@@ -86,34 +99,44 @@ public class Day17 {
         return ns;
     }
 
-    static List<Character> createEmptyRow(int y) {
+    static List<Character> createEmptyWWW(int w) {
         List<Character> ret = new ArrayList<>();
-        for (int j = 0; j < y; j++) {
+        for (int j = 0; j < w; j++) {
             ret.add('.');
         }
         return ret;
     }
 
-    static List<List<Character>> createEmptyLayer(int x, int y) {
+    static List<List<Character>> createEmptyRow(int y, int w) {
         List<List<Character>> ret = new ArrayList<>();
-        for (int i = 0; i < x; i++) {
-            ret.add(createEmptyRow(y));
+        for (int i = 0; i < y; i++) {
+            ret.add(createEmptyWWW(w));
         }
         return ret;
     }
 
-    static int countNeighbour(List<List<List<Character>>> space, int x, int y, int z) {
+    static List<List<List<Character>>> createEmptyLayer(int x, int y, int w) {
+        List<List<List<Character>>> ret = new ArrayList<>();
+        for (int i = 0; i < x; i++) {
+            ret.add(createEmptyRow(y, w));
+        }
+        return ret;
+    }
+
+    static int countNeighbour(List<List<List<List<Character>>>> space, int x, int y, int z, int w) {
         int count = 0;
         for (int i = -1; i <= 1; i++) { // z
             for (int j = -1; j <= 1; j++) { // x
                 for (int k = -1; k <= 1; k++) { // y
-                    if (i == j && j == k && k == 0) {
-                        // Skip self.
-                        continue;
-                    }
+                    for (int m = -1; m <= 1; m++) { // w
+                        if (i == j && j == k && k == m && m == 0) {
+                            // Skip self.
+                            continue;
+                        }
 
-                    if (checkActive(space, x + i, y + j, z + k)) {
-                        count ++;
+                        if (checkActive(space, x + i, y + j, z + k, w + m)) {
+                            count ++;
+                        }
                     }
                 }
             }
@@ -121,21 +144,26 @@ public class Day17 {
         return count;
     }
 
-    static boolean checkActive(List<List<List<Character>>> space, int x, int y, int z) {
+    static boolean checkActive(List<List<List<List<Character>>>> space, int x, int y, int z, int w) {
         if (z < 0 || space.size() <= z ||
                 x < 0 || space.get(0).size() <= x ||
-                y < 0 || space.get(0).get(0).size() <= y) {
+                y < 0 || space.get(0).get(0).size() <= y ||
+                w < 0 || space.get(0).get(0).get(0).size() <= w) {
             return false;
         }
-        return space.get(z).get(x).get(y) == '#';
+        return space.get(z).get(x).get(y).get(w) == '#';
     }
 
-    static List<List<List<Character>>> copy(List<List<List<Character>>> space) {
-        List<List<List<Character>>> ns = new ArrayList<>();
-        for (List<List<Character>> layer : space) {
-            List<List<Character>> lcopy = new ArrayList<>();
-            for (List<Character> row : layer) {
-                lcopy.add(new ArrayList<>(row));
+    static List<List<List<List<Character>>>> copy(List<List<List<List<Character>>>> space) {
+        List<List<List<List<Character>>>> ns = new ArrayList<>();
+        for (List<List<List<Character>>> layer : space) {
+            List<List<List<Character>>> lcopy = new ArrayList<>();
+            for (List<List<Character>> row : layer) {
+                List<List<Character>> rcopy = new ArrayList<>();
+                for (List<Character> www : row) {
+                    rcopy.add(new ArrayList<>(www));
+                }
+                lcopy.add(rcopy);
             }
             ns.add(lcopy);
         }
