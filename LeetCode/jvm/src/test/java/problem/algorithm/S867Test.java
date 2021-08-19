b237aec514e0e288387cb67dbe3855d898d4c099
MewX <axx+github@google.com>
2021-08-19 14:51:22 +1000

    Added maven support for the package

--- LeetCode/jvm/src/test/java/problem/algorithm/S867Test.java
+++ LeetCode/jvm/src/test/java/problem/algorithm/S867Test.java
@@ -2,19 +2,17 @@ package problem.algorithm;
 
 import org.testng.annotations.Test;
 
-import static org.testng.Assert.*;
-
-public class S867Test {
+public class S867Test extends TestBase {
 
     @Test
     public void testExample1() {
-        assertEquals(new int[][]{{1, 4, 7}, {2, 5, 8}, {3, 6, 9}},
+        assert2DArrayEqual(new int[][]{{1, 4, 7}, {2, 5, 8}, {3, 6, 9}},
                 new S867().transpose(new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}));
     }
 
     @Test
     public void testExample2() {
-        assertEquals(new int[][]{{1, 4}, {2, 5}, {3, 6}},
+        assert2DArrayEqual(new int[][]{{1, 4}, {2, 5}, {3, 6}},
                 new S867().transpose(new int[][]{{1, 2, 3}, {4, 5, 6}}));
     }
 }
--- LeetCode/jvm/src/test/java/problem/algorithm/S883Test.java
+++ LeetCode/jvm/src/test/java/problem/algorithm/S883Test.java
@@ -3,8 +3,6 @@ package problem.algorithm;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import static org.testng.Assert.*;
-
 public class S883Test {
 
     @Test
