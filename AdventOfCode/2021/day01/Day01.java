871aea0a289d2874c6c5926f59dde8c305b715fa
MewX <axx+github@google.com>
2021-12-01 19:03:39 +1100

    Added solution for AoD 2021 day01 part 2

--- AdventOfCode/2021/day01/Day01.java
+++ AdventOfCode/2021/day01/Day01.java
@@ -14,12 +14,25 @@ public class Day01 {
 
         // Do the calculation.
         System.out.println("q1: " + countIncreasing(sweepReports));
+        System.out.println("q2: " + countWindowIncreasing(sweepReports));
     }
 
     private static int countIncreasing(List<Integer> reports) {
         int count = 0;
         for (int i = 1; i < reports.size(); i++) {
             if (reports.get(i) > reports.get(i - 1)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private static int countWindowIncreasing(List<Integer> reports) {
+        int count = 0;
+        for (int i = 3; i < reports.size(); i++) {
+            int windowPrev = reports.get(i - 1) + reports.get(i - 2) + reports.get(i - 3);
+            int windowNow = reports.get(i) + reports.get(i - 1) + reports.get(i - 2);
+            if (windowNow > windowPrev) {
                 count++;
             }
         }
