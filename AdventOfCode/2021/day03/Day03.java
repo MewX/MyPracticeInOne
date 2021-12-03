edf1adf82ea84c5e9cf3626a89466e3675499a43
MewX <mewx@google.com>
2021-12-03 16:28:36 +1100

    Added AoD 2021 day03 part 2

--- AdventOfCode/2021/day03/Day03.java
+++ AdventOfCode/2021/day03/Day03.java
@@ -14,7 +14,7 @@ public class Day03 {
     }
     s.close();
 
-    System.out.println("part 1: ");
+    // part 1.
     String gamma = "", epsilon = "";
     for (int i = 0; i < input.get(0).length(); i++) {
       int c1 = 0;
@@ -36,5 +36,62 @@ public class Day03 {
       }
     }
     System.out.println("part 1: " + (Integer.parseInt(gamma, 2) * Integer.parseInt(epsilon, 2)));
+
+    // part 2.
+    System.out.println(
+        "part 2: " + (part2(new ArrayList<>(input), 0, '1') * part2(new ArrayList<>(input), 0,
+            '0')));
+  }
+
+  private static void dump(List<String> input) {
+    System.out.println("Dumping: ");
+    for (String s : input) {
+      System.out.println(s);
+    }
+  }
+
+  // private static String oxygen = "", co2 = "";
+  private static int part2(List<String> input, int index, char target) {
+    if (input.size() == 1) {
+      // Good!
+      return Integer.parseInt(input.get(0), 2);
+    }
+
+    // errors.
+    if (input.size() == 0) {
+      System.out.println("error, input 0; index is " + index);
+      System.exit(-1);
+    }
+    if (index >= input.get(0).length()) {
+      System.out.println("error, index too big");
+      dump(input);
+      System.exit(-2);
+    }
+
+    int c1 = 0;
+    for (String str : input) {
+      if (str.charAt(index) == '1') {
+        c1++;
+      }
+    }
+    int c0 = input.size() - c1;
+
+    char toKeep;
+    if (target == '1') {
+      // Oxygen.
+      toKeep = c1 >= c0 ? '1' : '0';
+    } else {
+      // CO2.
+      toKeep = c0 <= c1 ? '0' : '1';
+    }
+
+    for (int i = input.size() - 1; i >= 0; i--) {
+      if (input.get(i).charAt(index) != toKeep) {
+        input.remove(i);
+      }
+    }
+    System.out.println("index: " + index);
+    dump(input);
+    return part2(input, index + 1, target);
   }
 }
