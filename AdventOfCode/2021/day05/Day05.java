f4caca9ecc3e4e6bb7a520b571ea8ea484087be1
MewX <axx+github@google.com>
2021-12-05 16:40:37 +1100

    Added AoD 2021 day05 part 2

--- AdventOfCode/2021/day05/Day05.java
+++ AdventOfCode/2021/day05/Day05.java
@@ -65,14 +65,43 @@ public class Day05 {
         }
         dump(map, 10, 10);
         System.out.println("part 1: " + count);
+
+        // Part 2.
+        for (int i = 0; i < from.size(); i++) {
+            if (inDiagonal(from.get(i), to.get(i))) {
+//                System.out.format("Found diagonal %d, %d -> %d, %d\n",
+//                        from.get(i).x, from.get(i).y, to.get(i).x, to.get(i).y);
+                int xSign = to.get(i).x - from.get(i).x < 0 ? -1 : 1;
+                int ySign = to.get(i).y - from.get(i).y < 0 ? -1 : 1;
+                for (int diff = 0; diff <= Math.abs(to.get(i).x - from.get(i).x); diff++) {
+                    map[from.get(i).x + xSign * diff][from.get(i).y + ySign * diff]++;
+                }
+            }
+        }
+
+        count = 0;
+        for (int[] row : map) {
+            for (int cell : row) {
+                if (cell > 1) {
+                    count++;
+                }
+            }
+        }
+        dump(map, 10, 10);
+        System.out.println("part 2: " + count);
     }
 
     private static void dump(int[][] map, int x, int y) {
-        for (int i = 0; i < x; i++) {
-            for (int j = 0; j < y; j++) {
+        for (int j = 0; j < y; j++) {
+            for (int i = 0; i < x; i++) {
                 System.out.print("" + map[i][j] + " ");
             }
             System.out.println();
         }
+    }
+
+    private static boolean inDiagonal(Point a, Point b) {
+        int diffX = a.x - b.x, diffY = a.y - b.y;
+        return Math.abs(diffX) == Math.abs(diffY) && diffX != 0;
     }
 }
