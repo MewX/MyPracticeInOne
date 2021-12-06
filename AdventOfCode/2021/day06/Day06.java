021f48473d9484d41a1c60c86daf425eb7ec77c1
MewX <axx+github@google.com>
2021-12-06 16:31:19 +1100

    Added AoD 2021 day06 part 2

--- AdventOfCode/2021/day06/Day06.java
+++ AdventOfCode/2021/day06/Day06.java
@@ -16,13 +16,13 @@ public class Day06 {
         s.close();
 
         // Part 1.
-        final int DAYS = 80;
+        int DAYS = 80;
         List<Integer> timers = new ArrayList<>(initialTimers);
         for (int day = 0; day < DAYS; day++) {
             final int originalCount = timers.size();
             for (int i = 0; i < originalCount; i++) {
                 int lantern = timers.get(i);
-                lantern --;
+                lantern--;
                 if (lantern < 0) {
                     timers.set(i, 6);
                     timers.add(8);
@@ -36,11 +36,40 @@ public class Day06 {
         System.out.println("part 1: " + timers.size());
 
         // Part 2.
+        DAYS = 256;
+        Map<Integer, Long> timerToLantern = new HashMap<>(); // Timer to the number of lanterns.
+        for (Integer i : initialTimers) {
+            timerToLantern.put(i, timerToLantern.getOrDefault(i, 0L) + 1);
+        }
+        for (int day = 0; day < DAYS; day++) {
+            Map<Integer, Long> temp = new HashMap<>();
+            for (Integer timerKey : timerToLantern.keySet()) {
+                Long count = timerToLantern.get(timerKey);
+                timerKey--;
+                if (timerKey < 0) {
+                    temp.put(6, temp.getOrDefault(6, 0L) + count);
+                    temp.put(8, temp.getOrDefault(8, 0L) + count);
+                } else {
+                    temp.put(timerKey, temp.getOrDefault(timerKey, 0L) + count);
+                }
+            }
+            timerToLantern = temp;
+//            System.out.print("Day " + day + ": ");
+//            dumpMap(timerToLantern);
+        }
+        System.out.println("part 2: " + timerToLantern.values().stream().reduce(0L, Long::sum));
     }
 
     private static void dump(List<Integer> list) {
         for (Integer i : list) {
             System.out.print("" + i + ",");
+        }
+        System.out.println();
+    }
+
+    private static void dumpMap(Map<Integer, Long> timerToLantern) {
+        for (Integer key : timerToLantern.keySet()) {
+            System.out.print("(" + key + ": " + timerToLantern.get(key) + ")" + ", ");
         }
         System.out.println();
     }
