3dc060517720b06a99c35eb78d94a2240fe8cbd4
Amos Xia <axx+github@google.com>
2022-12-01 21:37:42 -0800

    Over-engineered solution for AoC 2022/02

--- AdventOfCode/2022/day02/RockPaperScissors.java
+++ AdventOfCode/2022/day02/RockPaperScissors.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
@@ -9,9 +11,24 @@ public class RockPaperScissors {
     ROCK, PAPER, SCISSOR
   }
 
+  private static final Map<Shape, Integer> SHAPE_SCORE = Map.of(Shape.ROCK, 1, Shape.PAPER, 2,
+      Shape.SCISSOR, 3);
+
+  // Key beats Value.
+  private static final Map<Shape, Shape> WINNING_MAP = Map.of(Shape.ROCK, Shape.PAPER, Shape.PAPER,
+      Shape.ROCK, Shape.SCISSOR, Shape.PAPER);
+
+  // Key loses Value.
+  private static final Map<Shape, Shape> LOSING_MAP = WINNING_MAP.entrySet().stream()
+      .collect(Collectors.toMap(
+          Entry::getValue, Entry::getKey));
+
   private enum Result {
     LOSE, DRAW, WIN
   }
+
+  private static final Map<Result, Integer> RESULT_SCORE = Map.of(Result.LOSE, 0, Result.DRAW, 3,
+      Result.WIN, 6);
 
   public static void main(String[] args) {
     // A for Rock, B for Paper, and C for Scissors.
@@ -58,14 +75,13 @@ public class RockPaperScissors {
     // Q1.
     int score = 0;
     for (int round = 0; round < opChoices.size(); round++) {
-      // Check shape.
-      score += getShapeScore(myChoices.get(round));
+      score += SHAPE_SCORE.get(myChoices.get(round));
 
       // Check result.
       if (checkMyWin(opChoices.get(round), myChoices.get(round))) {
-        score += 6;
+        score += RESULT_SCORE.get(Result.WIN);
       } else if (opChoices.get(round) == myChoices.get(round)) {
-        score += 3;
+        score += RESULT_SCORE.get(Result.DRAW);
       }
     }
     System.out.println("Q1: " + score);
@@ -92,64 +108,25 @@ public class RockPaperScissors {
       Shape myShape = Shape.ROCK;
       switch (mappedChoices.get(round)) {
         case LOSE:
-          myShape = getLose(opChoices.get(round));
+          myShape = WINNING_MAP.get(opChoices.get(round));
           break;
         case WIN:
-          score += 6;
-          myShape = getWin(opChoices.get(round));
+          myShape = LOSING_MAP.get(opChoices.get(round));
           break;
         case DRAW:
-          score += 3;
           myShape = opChoices.get(round);
           break;
       }
 
-      // Check shape.
-      score += getShapeScore(myShape);
+      score += RESULT_SCORE.get(mappedChoices.get(round));
+      score += SHAPE_SCORE.get(myShape);
     }
     System.out.println("Q2: " + score);
 
   }
 
   private static boolean checkMyWin(Shape op, Shape mine) {
-    return op == Shape.ROCK && mine == Shape.PAPER || op == Shape.PAPER && mine == Shape.SCISSOR
-        || op == Shape.SCISSOR && mine == Shape.ROCK;
-  }
-
-  private static int getShapeScore(Shape shape) {
-    switch (shape) {
-      case ROCK:
-        return 1;
-      case PAPER:
-        return 2;
-      case SCISSOR:
-        return 3;
-    }
-    return 0;
-  }
-
-  private static Shape getLose(Shape op) {
-    switch (op) {
-      case ROCK:
-        return Shape.SCISSOR;
-      case PAPER:
-        return Shape.ROCK;
-      case SCISSOR:
-        return Shape.PAPER;
-    }
-    return Shape.ROCK;
-  }
-
-  private static Shape getWin(Shape op) {
-    switch (op) {
-      case ROCK:
-        return Shape.PAPER;
-      case PAPER:
-        return Shape.SCISSOR;
-      case SCISSOR:
-        return Shape.ROCK;
-    }
-    return Shape.ROCK;
+    return op == WINNING_MAP.get(mine);
   }
 
 }
