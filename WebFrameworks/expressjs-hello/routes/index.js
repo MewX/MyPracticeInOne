d28c0b24f6ebb0875668d5b7b24cf084d086ea02
MewX <xiayuanzhong+gpg2020@gmail.com>
2023-06-04 23:27:34 +1000

    Added routers for express.js hello world

--- WebFrameworks/expressjs-hello/routes/index.js
+++ WebFrameworks/expressjs-hello/routes/index.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Express'});
+});
+
+// Get something else.
+router.get('/hello', function (req, res, next) {
+    res.render('layout', {title: 'Hello Express.js', content: 'Hello contents.'});
 });
 
 module.exports = router;
