2a893f476c9a36ae3329a4f4ed023036bc47f4aa
Amos Xia <axx+github@google.com>
2023-11-30 22:51:50 -0800

    Added AoC 2023 day01 part 2 unfinished solution (and I hate Java!!!)

--- AdventOfCode/2023/day01/Day01.java
+++ AdventOfCode/2023/day01/Day01.java
@@ -30,5 +30,37 @@ public class Day01 {
         }
 
         System.out.println("part 1: " + result);
+
+        result = 0;
+        p = Pattern.compile("(\\d|one|two|three|four|five|six|seven|eight|nine)");
+        for (String str : input) {
+            List<Integer> digits = new ArrayList<>();
+            Matcher m = p.matcher(str);
+            while (m.find()) {
+                String t = m.group();
+                System.out.println("Added " + t + " -> " + parseInt(t));
+                digits.add(parseInt(t));
+            }
+            result += digits.get(0) * 10 + digits.get(digits.size() - 1);
+        }
+        System.out.println("part 2: " + result);
+    }
+
+    private static int parseInt(String str) {
+        ArrayList<String> table = new ArrayList<>();
+        table.add("one");
+        table.add("two");
+        table.add("three");
+        table.add("four");
+        table.add("five");
+        table.add("six");
+        table.add("seven");
+        table.add("eight");
+        table.add("nine");
+        if (table.contains(str)) {
+            return 1 + table.indexOf(str);
+        } else {
+            return Integer.parseInt(str);
+        }
     }
 }
