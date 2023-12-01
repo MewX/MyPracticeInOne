908a41129327c4ef49f915f650a94fb9c0e6e33f
Amos Xia <axx+github@google.com>
2023-11-30 23:31:02 -0800

    Added AoC 2023 day01 part 2 stupid solution

--- AdventOfCode/2023/day01/Day01.java
+++ AdventOfCode/2023/day01/Day01.java
@@ -1,66 +1,68 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class Day01 {
+
+    private static final List<String> WORD_TABLE = Arrays.asList("one", "two", "three", "four",
+        "five", "six", "seven", "eight", "nine");
+
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         List<String> input = new ArrayList<>();
-        while (s.hasNextLine()) {
-            String line = s.nextLine().trim();
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine().trim().toLowerCase();
             if (line.isEmpty()) {
                 continue;
             }
             input.add(line);
         }
-        s.close();
+       scanner.close();
 
         Pattern p = Pattern.compile("\\d");
 
         int result = 0;
-        for (String str : input) {
-            List<Integer> digits = new ArrayList<>();
-            Matcher m = p.matcher(str);
-            while (m.find()) {
-                digits.add(Integer.parseInt(m.group()));
-            }
-            result += digits.get(0) * 10 + digits.get(digits.size() - 1);
-        }
+        // for (String str : input) {
+        //     List<Integer> digits = new ArrayList<>();
+        //     Matcher m = p.matcher(str);
+        //     while (m.find()) {
+        //         digits.add(Integer.parseInt(m.group()));
+        //     }
+        //     result += digits.get(0) * 10 + digits.get(digits.size() - 1);
+        // }
+        //
+        // System.out.println("part 1: " + result);
 
-        System.out.println("part 1: " + result);
-
+        // OK, writing this code will get me fired by Google.
         result = 0;
-        p = Pattern.compile("(\\d|one|two|three|four|five|six|seven|eight|nine)");
         for (String str : input) {
-            List<Integer> digits = new ArrayList<>();
-            Matcher m = p.matcher(str);
-            while (m.find()) {
-                String t = m.group();
-                System.out.println("Added " + t + " -> " + parseInt(t));
-                digits.add(parseInt(t));
+            for (int i = 1; i <= 9; i++) {
+                str = str.replace("" + i, WORD_TABLE.get(i - 1));
             }
-            result += digits.get(0) * 10 + digits.get(digits.size() - 1);
+            System.out.println("After: " + str);
+
+            String s = str;
+            List<Integer> firstWordIndex = WORD_TABLE.stream()
+                .map(w -> s.contains(w) ? s.indexOf(w) : 1000).collect(Collectors.toList());
+            int firstNumber = 1 + WORD_TABLE.indexOf(WORD_TABLE.stream()
+                .min(Comparator.comparingInt(a -> firstWordIndex.get(WORD_TABLE.indexOf(a)))).get());
+            System.out.println("First " + firstWordIndex + " -> " + firstNumber);
+
+            List<Integer> lastWordIndex = WORD_TABLE.stream()
+                .map(w -> s.contains(w) ? s.lastIndexOf(w) : -1).collect(Collectors.toList());
+            int lastNumber = 1 + WORD_TABLE.indexOf(WORD_TABLE.stream()
+                .max(Comparator.comparingInt(a -> lastWordIndex.get(WORD_TABLE.indexOf(a)))).get());
+            System.out.println("Last " + lastWordIndex + " -> " + lastNumber);
+
+            int num = firstNumber * 10 + lastNumber;
+            System.out.println("Added " + num);
+            result += num;
         }
         System.out.println("part 2: " + result);
-    }
-
-    private static int parseInt(String str) {
-        ArrayList<String> table = new ArrayList<>();
-        table.add("one");
-        table.add("two");
-        table.add("three");
-        table.add("four");
-        table.add("five");
-        table.add("six");
-        table.add("seven");
-        table.add("eight");
-        table.add("nine");
-        if (table.contains(str)) {
-            return 1 + table.indexOf(str);
-        } else {
-            return Integer.parseInt(str);
-        }
     }
 }
